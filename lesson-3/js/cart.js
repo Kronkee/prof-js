a932b06d8bfe24b2e84c21cffadef262e434ac60
Екатерина Богопольская <Katya.sovie@gmail.com>
2022-04-04 18:43:22 +0300

    add html, css, js for lesson-3

---
+++ lesson-3/js/cart.js
@@ -0,0 +1,85 @@
+
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+class CartList {
+    constructor(container = '.basket'){
+        this.container = container;
+        this.goods = [];//массив товаров из JSON документа
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = data.contents;
+//                 console.log(data);
+                this.render()
+            });
+    }
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts(){
+
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new CartItem(product);
+//            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+
+
+class CartItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render(){
+        return `<div class="cart-item" data-id="${this.id}">
+                <div class="cart-item_element">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="delete-btn">x</button>
+                </div>
+            </div>`
+    }
+}git checkout -b lesson-3
+
+let cart = new CartList();
+console.log(list.allProducts);
+
+let basket = document.querySelector('.basket')
+let btn = document.querySelector('.btn-cart');
+btn.addEventListener('click', function () {
+    basket.classList.toggle('hidden')
+});
